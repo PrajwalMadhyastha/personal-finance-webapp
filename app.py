df6e7db745aac59d5d05c99e8076737aa980d99e
PrajwalMadhyastha <prajwalmadhyastha777@gmail.com>
2025-06-03 18:18:41 +0530

    feat: Implement Python control flow (if/elif/else, loops) and basic functions for expense logging

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
-print("Personal Finance Web App - Initial Setup Complete!")
+try:
+    category = input("Enter expense category: ")
+except ValueError:
+    print("Invalid category. Please enter a category.")
+    category = ''
+
+if category.lower() == "food":
+    print("Remember to track your grocery bills!")
+elif category.lower() == "transport":
+    print("Remember to track your commut expenses!")
+elif category.lower() == "entertainment":
+    print("Remember to track your Entertainment bills!")
+
+try:
+    amount = float(input("Enter expense amount: "))
+except ValueError:
+    print("Invalid amount. Please enter a number.")
+    amount = 0
+
+if amount <= 0:
+    print("Amount is not greater than zero")
+
+categories = ['Food', 'Transport', 'Bills']
+
+for i in categories:
+    print(i)
+
+def greet_user(name):
+    print("Hello " + name)
+
+def log_new_expense(category, amount, description):
+    return ("Category is " + category + "Description is " + description + "and the Amount is " + amount)
+
