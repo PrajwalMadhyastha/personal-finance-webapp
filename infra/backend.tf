308e7bf8fcf580f27b9b26e77ec3521bc6dd1aa1
PrajwalMadhyastha <prajwalmadhyastha777@gmail.com>
2025-06-07 17:37:37 +0530

    feat: Configure Terraform remote state backend

---
+++ infra/backend.tf
@@ -0,0 +1,12 @@
+# infra/backend.tf
+
+terraform {
+  backend "azurerm" {
+    # Replace these names with the ones you used in the 'az' commands above
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "pfatfstatefinanceapp"
+    container_name       = "tfstate"
+    # This will be the name of the state file inside the container
+    key                  = "prod.terraform.tfstate"
+  }
+}
