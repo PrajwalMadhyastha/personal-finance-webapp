7a64ea0067113b7fb2e8fae48acc24a3f76143eb
PrajwalMadhyastha <prajwalmadhyastha777@gmail.com>
2025-06-10 14:32:42 +0530

    fix: downsized database to avoid cost

--- infra/database.tf
+++ infra/database.tf
@@ -1,19 +1,12 @@
+
 # infra/database.tf
-
-# This data source gets your current public IP address.
-# We use a service that specifically returns the IPv4 address.
-# data "http" "myip" {
-#   url = "https://ipv4.icanhazip.com"
-# }
-
-# Generates a random string to ensure the SQL server name is globally unique.
+# NOTE: The 'data "http" "myip"' and 'local_access' firewall rule have been removed as per our previous discussion.
 resource "random_string" "unique" {
   length  = 6
   special = false
   upper   = false
 }
-
-# --- Azure SQL Server (Logical Server) ---
+# --- Azure SQL Server ---
 resource "azurerm_mssql_server" "sql_server" {
   name                         = "pfa-sql-server-${random_string.unique.result}"
   resource_group_name          = azurerm_resource_group.rg.name
@@ -21,25 +14,20 @@ resource "azurerm_mssql_server" "sql_server" {
   version                      = "12.0"
   administrator_login          = var.db_admin_login
   administrator_login_password = var.db_admin_password
-
   public_network_access_enabled = true
 }
-
 # --- Azure SQL Database ---
 resource "azurerm_mssql_database" "sql_database" {
   name      = "finance_db"
   server_id = azurerm_mssql_server.sql_server.id
-  sku_name  = "Basic"
+  sku_name = "GP_S_Gen5_1"
+  auto_pause_delay_in_minutes = 60
+  min_capacity                = 0.5
+  max_size_gb                 = 32
+  # This explicitly disables Geo-redundant backups for this specific database.
+  geo_backup_enabled = false
 }
-
-# --- Azure SQL Firewall Rules ---
-# resource "azurerm_mssql_firewall_rule" "local_access" {
-#   name             = "AllowMyIP"
-#   server_id        = azurerm_mssql_server.sql_server.id
-#   start_ip_address = chomp(data.http.myip.response_body)
-#   end_ip_address   = chomp(data.http.myip.response_body)
-# }
-
+# --- Azure SQL Firewall Rule for Azure Services ---
 resource "azurerm_mssql_firewall_rule" "azure_access" {
   name             = "AllowAllWindowsAzureIps"
   server_id        = azurerm_mssql_server.sql_server.id
